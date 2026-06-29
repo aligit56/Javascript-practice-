6ad810d4271fc15b04add19f30a700219da62431
Muhammad Ali <alisheikx345@gmail.com>
2026-06-29 16:34:46 +0000

    comparison lecture

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1 );
+// console.log(2 == 1 );
+// console.log(2 != 1 );
+
+
+// problem came when we try to 
+// compare the two differnet datatypes 
+
+ console.log("2" > 1 );
+ console.log("002" > 1 );
+
+console.log(null > 0);
+console.log(null == 0 );
+console.log(null>= 0 );
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// should avoid to do the above all 
+
+// === checks strictly 
+
+ console.log("2"=== 2);
